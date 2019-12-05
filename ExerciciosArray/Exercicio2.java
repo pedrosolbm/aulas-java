4a44ac2bf7591f27fe2a0bc8aee01402e9ba41e7
Guilherme Abranches <guilherme.abranches@outlook.com>
2019-12-05 10:45:58 -0300

    Create Exercicio2.java

---
+++ ExerciciosArray/Exercicio2.java
@@ -0,0 +1,24 @@
+/*
+ * Escreva um trecho Java que exiba os valores de um 
+ * Array a Double numa mesma linha.
+*/
+
+/* 
+  * Programadores: Pedro Sol B Montes, Guilherme A. Dias
+  * Data: 5/12/2019
+*/
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        // declaração do vetor do tipo int com 10 posições (0 até 9)
+        int vetor[] = new int[10];
+
+        // contagem regressiva de 9 até 0
+        for (int i = 0; i <= 9; i++) {
+            // para cada repetição, a posição do vetor vai receber (i + 1)
+            vetor[i] = i + 1;
+            // imprime o valor do vetor
+            System.out.print(vetor[i]+" "); 
+        }
+    }
+}
