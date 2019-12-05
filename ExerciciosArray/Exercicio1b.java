9d0703e44e5cea87c451643021d92ed1c39ad4ff
Pedro Montes <pedrosolbm@gmail.com>
2019-12-05 10:40:43 -0300

    refeito

--- ExerciciosArray/Exercicio1b.java
+++ ExerciciosArray/Exercicio1b.java
@@ -1,6 +1,10 @@
-/**
- * Para cada conjunto de valores abaixo, escreva o código Java, usando laço(s),
- * que preencha um array com os valores: a) 10 9 8 7 6 5 4 3 2 1
+/*
+Para cada conjunto de valores abaixo, escreva o código Java, usando laço(s),
+que preencha um array com os valores: b) 0 1 4 9 16 25 36 49 64 81 100
+ */
+/*
+ * Programadores: Pedro Sol B Montes, Guilherme A. Dias
+ * Data: 5/12/2019
  */
 public class Exercicio1b {
     public static void main(String[] args) {
@@ -10,9 +14,12 @@ public class Exercicio1b {
 
         // contagem regressiva de 9 até 0
         for (int i = 0; i <= 9; i++) {
+            
             // para cada repetição, a posição do vetor vai receber (i*i)
             vetor[i] = i * i;
-            System.out.print(vetor[i]+" "); // imprime o valor do vetor
+
+            // imprime o valor do vetor
+            System.out.print(vetor[i]+" "); 
         }
     }
 }
--- ExerciciosArray/Exercicio1c.java
+++ ExerciciosArray/Exercicio1c.java
@@ -1,6 +1,9 @@
+//Para cada conjunto de valores abaixo, escreva o código Java, usando laço(s),
+//que preencha um array com os valores: c) 1 2 3 4 5 10 20 30 40 50
+
 /**
- * Para cada conjunto de valores abaixo, escreva o código Java, usando laço(s),
- * que preencha um array com os valores: a) 10 9 8 7 6 5 4 3 2 1
+ * Programadores: Pedro Sol B Montes, Guilherme A. Dias
+ * Data: 5/12/2019
  */
 public class Exercicio1c {
     public static void main(String[] args) {
@@ -8,8 +11,20 @@ public class Exercicio1c {
         // declaração do vetor do tipo int com 10 posições (0 até 9)
         int vetor[] = new int[10];
 
-       
-           
-           
+        //contador que vai de 0 até a ultima posição do vetor
+        for (int i = 0; i < vetor.length; i++) {
+
+            // para cada repetição, a posição do vetor recebe o numero do contador + 1
+            vetor[i] = i + 1;
+
+            // a partir de 5, o contador multiplica o resultado pelos primeiros numeros
+            if (i >= 5) {
+                vetor[i] = 10*((i-5)+1);
+            }
+
+            //Exibe os valores do vetor
+            System.out.print(vetor[i]+" ");
+        }
+
     }
 }
