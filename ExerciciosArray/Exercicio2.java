20ee36189156f586d75d5cdd3076809ed6421755
Guilherme Abranches <guilherme.abranches@outlook.com>
2019-12-05 11:11:50 -0300

    Update Exercicio2.java

--- ExerciciosArray/Exercicio2.java
+++ ExerciciosArray/Exercicio2.java
@@ -11,14 +11,14 @@ public class Exercicio2 {
     public static void main(String[] args) {
 
         // declaração do vetor do tipo int com 10 posições (0 até 9)
-        int vetor[] = new int[10];
+        double a[] = new double[10];
 
         // contagem regressiva de 9 até 0
         for (int i = 0; i <= 9; i++) {
             // para cada repetição, a posição do vetor vai receber (i + 1)
-            vetor[i] = i + 1;
+            a[i] = i + 1;
             // imprime o valor do vetor
-            System.out.print(vetor[i]+" "); 
+            System.out.print(a[i]+" ");
         }
     }
 }
