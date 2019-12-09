a0cfaca0960ee32b117161ac4d9ea03b01551434
Pedro Montes <pedrosolbm@gmail.com>
2019-12-09 11:01:47 -0300

    Update Exercicio11.java

--- ExerciciosRecursividade/Exercicio11.java
+++ ExerciciosRecursividade/Exercicio11.java
@@ -11,24 +11,25 @@ import java.util.Scanner;
 public class Exercicio11 {
 
     public static void main(String[] args) {
-        
+
         // declaração de variáveis
         Scanner ler = new Scanner(System.in);
         int num;
         int resultado;
 
-        //Solicita entrada de dados via teclado
+        // Solicita entrada de dados via teclado
         System.out.print("Digite um numero: ");
         num = ler.nextInt();
 
-        //Armazena o retorno da função recursiva na variável resultado
+        // Armazena o retorno da função recursiva na variável resultado
         resultado = fatorial(num);
 
-        //Imprime o resultado na tela
+        // Imprime o resultado na tela
         System.out.println("O fatorial de " + num + " é " + resultado);
 
     }
-    //função recursiva para calculo de fatorial
+
+    // função recursiva para calculo de fatorial
     public static int fatorial(int num) {
         if (num == 0) {
             return 1;
