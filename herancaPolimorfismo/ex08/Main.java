f5ffe195a01a61a7780e6ecc193f7068de9ac12a
Pedro Montes <pedrosolbm@gmail.com>
2019-12-13 12:57:48 -0300

    fixed

--- herancaPolimorfismo/ex08/Main.java
+++ herancaPolimorfismo/ex08/Main.java
@@ -1,33 +1,44 @@
+
+/* 
+8. Crie uma classe chamada Ingresso que possui um valor em reais e um método imprimeValor().
+crie uma classe VIP, que herda Ingresso e possui um valor adicional. Crie um método que
+retorne o valor do ingresso VIP (com o adicional incluído).
+crie uma classe Normal, que herda Ingresso e possui um método que imprime: "Ingresso
+Normal".
+crie uma classe CamaroteInferior (que possui a localização do ingresso e métodos para acessar
+e imprimir esta localização) e uma classe CamaroteSuperior, que é mais cara (possui valor
+adicional). Esta última possui um método para retornar o valor do ingresso. Ambas as classes
+herdam a classe VIP.
+*/
+/*
+ * Programadores: Pedro Sol B. Montes, Guilherme A. Dias 
+ * Data:11/12/2019
+ */
 public class Main {
 
 	public static void main(String[] args) {
-	
-		
+
 		Ingresso ingresso = new Ingresso();
 		Normal normal = new Normal();
 		VIP vip = new VIP();
 		CamaroteInferior ci = new CamaroteInferior();
 		CamaroteSuperior cs = new CamaroteSuperior();
-		
+
 		ingresso.setValor(120.00);
-		
+
 		normal.setValor(120.00);
 		normal.imprimeIngressoNormal();
 		System.out.println();
-		
+
 		vip.setAdicionalVip(40);
 		vip.imprimeIngressoVip(normal.getValor());
-		
+
 		ci.setLocalizacao("O camarote Inferior fica no segundo andar sala 3b");
 		ci.imprimeLocalizacao();
 		System.out.println();
-		
+
 		cs.setMasterVip(50);
 		cs.imprimeMasterVip(vip.getAdicionalVip());
-		
-		
-		
-		
-		
-	}	
+
+	}
 }
--- herancaPolimorfismo/ex9/Main.java
+++ herancaPolimorfismo/ex9/Main.java
@@ -1,21 +1,28 @@
+/* Crie a classe Imovel, que possui um endereço e um preço.
+crie uma classe Novo, que herda Imovel e possui um adicional no preço. Crie
+métodos de acesso e impressão deste valor adicional.
+crie uma classe Velho, que herda Imovel e possui um desconto no preço. Crie métodos de
+acesso e impressão para este desconto. */
+/*
+ * Programadores: Pedro Sol B. Montes, Guilherme A. Dias 
+ * Data:11/12/2019
+ */
 public class Main {
 	public static void main(String[] args) {
-		
-		
+
 		ImovelNovo in = new ImovelNovo();
 		ImovelVelho iv = new ImovelVelho();
-		
+
 		in.setEndereco("Rua dos milion�rios, 777");
 		in.setPreco(550300);
-		in.setValorAdicional(in.getPreco()*0.03);
+		in.setValorAdicional(in.getPreco() * 0.03);
 		System.out.println(in.toString());
 		in.valorImovelNovo(in.getPreco());
-		
-		
+
 		iv.setEndereco("Rua dos maltrapilhos, 888");
 		iv.setPreco(100500);
-		iv.setDescontoValor(iv.getPreco()*0.03);
-		System.out.println("\n\n"+iv.toString());
+		iv.setDescontoValor(iv.getPreco() * 0.03);
+		System.out.println("\n\n" + iv.toString());
 		iv.valorImovelVelho(iv.getPreco());
 	}
 }
