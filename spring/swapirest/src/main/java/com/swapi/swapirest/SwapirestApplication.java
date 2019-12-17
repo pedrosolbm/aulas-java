1eee73e1b371bcc6ab22d53e036e50ad061c1c24
Pedro Montes <pedrosolbm@gmail.com>
2019-12-17 12:41:09 -0300

    swapi

---
+++ spring/swapirest/src/main/java/com/swapi/swapirest/SwapirestApplication.java
@@ -0,0 +1,13 @@
+package com.swapi.swapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapirestApplication.class, args);
+	}
+
+}
---
+++ spring/swapirest/src/test/java/com/swapi/swapirest/SwapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapi.swapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
