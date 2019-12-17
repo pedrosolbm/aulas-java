2d7dd9d3bda70fb4874d9d81440e48fb1d6a24a3
Pedro Montes <pedrosolbm@gmail.com>
2019-12-17 10:31:56 -0300

    biding

---
+++ Angular/binding/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AlertModule } from 'ngx-bootstrap';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent,
+    InputPropertyComponent,
+    OutputPropertyComponent,
+    CicloComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AlertModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- Angular/hello-typescript/hello-typescript/main.ts
+++ Angular/hello-typescript/hello-typescript/main.ts
@@ -1,17 +1,23 @@
-
+// declarando uma variavel constante
 const minhaVar = 'minha variavel';
 
+//declarando uma função, que recebe dois parametros
 function minhaFunc(x, y) {
+    //retorna a soma dos parametros recebidos
     return x + y;
 }
 
-//es6 ou ecmascript
+//let é um tipo de variavel que não pode ser redeclarado como var
 let num = 2;
+
+ // declarando uma constante, não pode ser redeclarada nem ter seu valor alterado
 const PI = 3.14;
 
+// declaração de vetor
 var numeros = [1, 2, 3];
 
 //javascript
+//chama o metodo .map para alterar o valor no vetor
 numeros.map(function(valor){
     return valor * 2;
 });
@@ -20,22 +26,11 @@ numeros.map(function(valor){
 numeros.map(valor => valor * 2); //es 2015
 
 //teste da função matematica em ecmascript
+
+// declaração de classe,
 class Matematica {
+    //classe matematica tem uma função soma que recebe dois numeros
     soma(x, y){
         return x + y;
     }
 }
-
-/* exemplo de decorator que já pode ser carregado no babel
-@frozen class Foo {
-    @configurable(false) @enumerable(true) method() {}
-  } */
-
-//typescript é bom para definição do tipo de classe
-//como tipar a variavel
-//var n1: string = 'sjsjshd';
-//n1 = 4; //tipando uma variavel como string, por exemplo, ela nào receberá valor de inteiro
-//a não ser que a variavel tenha n
-//var n1 = 'sjsjshd';
-//n1 = 4; 
-
