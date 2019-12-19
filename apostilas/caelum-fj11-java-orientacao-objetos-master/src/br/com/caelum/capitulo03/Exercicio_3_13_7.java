a77ca77dc67a7a86305beda11a6fb0f47c97b178
Pedro Montes <pedrosolbm@gmail.com>
2019-12-19 14:41:38 -0300

    caelum

--- apostilas/caelum-fj11-java-orientacao-objetos-master/src/br/com/caelum/capitulo03/Exercicio_3_13_7.java
+++ apostilas/caelum-fj11-java-orientacao-objetos-master/src/br/com/caelum/capitulo03/Exercicio_3_13_7.java
@@ -3,14 +3,25 @@ package br.com.caelum.capitulo03;
 public class Exercicio_3_13_7 {
 
 	public static void main(String[] args) {
+
+		// declarando variaveis
 		int x = 13;
-		
+
+		// enquanto x for diferente de 1
 		while (x != 1) {
+
+			// se x for par
 			if (x % 2 == 0) {
+
+				// x recebe x dividido por 2
 				x = x / 2;
+
+				// se não
 			} else {
+				// x recebe x dividido por 3
 				x = 3 * x + 1;
 			}
+			// imprime x
 			System.out.print(x + "->");
 		}
 	}
--- apostilas/caelum-fj11-java-orientacao-objetos-master/src/br/com/caelum/capitulo03/Exercicio_3_13_8.java
+++ apostilas/caelum-fj11-java-orientacao-objetos-master/src/br/com/caelum/capitulo03/Exercicio_3_13_8.java
@@ -3,11 +3,18 @@ package br.com.caelum.capitulo03;
 public class Exercicio_3_13_8 {
 
 	public static void main(String[] args) {
-		
+
+		// contador de 1 até 4
 		for (int i = 1; i <= 4; i++) {
+
+			// contador de 1 até o numero do contador acima
 			for (int j = 1; j <= i; j++) {
+
+				// imprime o valor de 1xj junto com um espaço
 				System.out.print(i * j + " ");
 			}
+
+			// imprime um espaço em branco e pula linha
 			System.out.println(" ");
 		}
 	}
